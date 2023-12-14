5c55550cefb2350c34f6352167908d62e597364e
Ruslan Makhanov <kapitannelson.wf@gmail.com>
2023-12-14 18:02:08 +0200

    [User MS] Update user model

--- user-service/src/main/java/com/sytoss/ecommerce/platform/userservice/model/User.java
+++ user-service/src/main/java/com/sytoss/ecommerce/platform/userservice/model/User.java
@@ -17,12 +17,18 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID uid;
+    @Column(unique = true, nullable = false)
     private String login;
+    @Column(nullable = false)
     private String password;
+    @Column(nullable = false)
     private String email;
+    @Column(nullable = false)
     private String firstName;
+    @Column(nullable = false)
     private String lastName;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private UserRole role;
 }
