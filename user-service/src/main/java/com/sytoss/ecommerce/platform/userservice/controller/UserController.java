5c95b82e2a400e479be6704959dfebff96d50547
Ruslan Makhanov <kapitannelson.wf@gmail.com>
2023-12-14 14:26:08 +0200

    [User MS] Add correct POST status and location

--- user-service/src/main/java/com/sytoss/ecommerce/platform/userservice/controller/UserController.java
+++ user-service/src/main/java/com/sytoss/ecommerce/platform/userservice/controller/UserController.java
@@ -4,8 +4,11 @@ import com.sytoss.ecommerce.platform.userservice.model.User;
 import com.sytoss.ecommerce.platform.userservice.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,8 +17,15 @@ import java.util.UUID;
 public class UserController {
     private UserRepository userRepository;
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
-        userRepository.save(user);
+    public ResponseEntity<Void> createUser(@RequestBody User user) {
+        User savedUser = userRepository.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{uid}")
+                .buildAndExpand(savedUser.getUid())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
     @GetMapping("/users")
