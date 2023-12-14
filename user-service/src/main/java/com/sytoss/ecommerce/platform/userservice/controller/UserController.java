08a55a819dcc1a9302b28569cbaf470548354444
Ruslan Makhanov <kapitannelson.wf@gmail.com>
2023-12-14 18:13:57 +0200

    [User MS] Update user controller

--- user-service/src/main/java/com/sytoss/ecommerce/platform/userservice/controller/UserController.java
+++ user-service/src/main/java/com/sytoss/ecommerce/platform/userservice/controller/UserController.java
@@ -35,7 +35,7 @@ public class UserController {
     }
 
     @GetMapping("/{uid}")
-    public User getUserByUid(@PathVariable UUID uid) {
-        return userRepository.findById(uid).orElse(null);
+    public ResponseEntity<User> getUserByUid(@PathVariable UUID uid) {
+        return userRepository.findById(uid).map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
