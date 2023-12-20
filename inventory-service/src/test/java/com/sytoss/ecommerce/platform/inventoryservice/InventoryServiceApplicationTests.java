e20c1c3debbc4810db211a5a67fe67fdbb33a4ed
Ruslan Makhanov <kapitannelson.wf@gmail.com>
2023-12-20 16:50:05 +0200

    [Inventory MS] Init service

---
+++ inventory-service/src/test/java/com/sytoss/ecommerce/platform/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sytoss.ecommerce.platform.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
