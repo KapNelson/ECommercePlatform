907d63befeb66b107a4ef80849dfcc96d78edd8b
Ruslan Makhanov <kapitannelson.wf@gmail.com>
2024-01-05 15:36:16 +0200

    [Fulfillment MS] Init service

---
+++ fulfillment-service/src/main/java/com/sytoss/ecommerce/platform/fulfillmentservice/FulfillmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sytoss.ecommerce.platform.fulfillmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulfillmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FulfillmentServiceApplication.class, args);
+	}
+
+}
---
+++ fulfillment-service/src/test/java/com/sytoss/ecommerce/platform/fulfillmentservice/FulfillmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sytoss.ecommerce.platform.fulfillmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FulfillmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
